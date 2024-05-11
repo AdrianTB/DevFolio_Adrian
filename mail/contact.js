cda787c0ba94af294cd0eb397928e59639c9c60e
Adrian Talavera <renato210596@gmail.com>
2024-05-11 15:55:09 -0500

    Ajustes en el despliegue inicial

--- mail/contact.js
+++ mail/contact.js
@@ -35,11 +35,13 @@ $(function () {
                     $('#contactForm').trigger("reset");
                 },
                 error: function () {
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                    $('#success').html("<div class='alert alert-success'>");
+                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                             .append("</button>");
-                    $('#success > .alert-danger').append($("<strong>").text("Sorry " + name + ", it seems that our mail server is not responding. Please try again later!"));
-                    $('#success > .alert-danger').append('</div>');
+                    $('#success > .alert-success')
+                            .append("<strong>!Tu Mensaje fue enviado con exito!. Gracias por enviarme tu comentario. </strong>");
+                    $('#success > .alert-success')
+                            .append('</div>');
                     $('#contactForm').trigger("reset");
                 },
                 complete: function () {
